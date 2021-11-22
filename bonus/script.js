38711857584e3ab5df9ad5072eab5a3f1cbc273f
scollo26 <giovanniscollo26@gmail.com>
2021-11-22 17:57:07 +0100

    bonus-aggiunta -li-color-background

---
+++ bonus/script.js
@@ -0,0 +1,55 @@
+// variabile-hamburger
+const hamburger = document.querySelector('.hamburger-menu');
+console.log(hamburger);
+
+// variabile-active
+const active = document.querySelector('.header-right a i');
+console.log(active);
+
+// variabile-desactive
+const desactive = document.querySelector('.hamburger-menu a i');
+console.log(desactive);
+
+
+const blue = document.getElementById('.blue');
+const red = document.getElementById('.red');
+
+
+
+const aggiungi = document.querySelector('.aggiungi');
+const rimuovi = document.querySelector('.rimuovi');
+const add = document.querySelector('.add');
+
+const li = document.createElement('li');
+
+// click-tasto-hamburger
+
+active.addEventListener('click', function(){
+
+    
+    hamburger.classList.add('active');
+    console.log(hamburger.className);
+    hamburger.style.backgroundColor = "grey";
+    hamburger.style.color = "blue"; 
+    
+});
+
+aggiungi.addEventListener('click', function(){
+    li.append("prova");
+    add.append(li);
+});
+
+rimuovi.addEventListener('click', function(){
+    li.innerHTML = '';
+});
+
+
+
+
+// click-tasto-x
+
+desactive.addEventListener('click', function(){
+
+    hamburger.classList.remove('active');
+    console.log(hamburger.className);
+});
